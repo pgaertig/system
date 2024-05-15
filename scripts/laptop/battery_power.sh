fc1aae3c41f3b58ebbe1fe141023cf3eee6c4586
Piotr Gaertig <github@gaertig.pl>
2024-05-15 14:44:42 +0200

    Laptop battery discharge indicator - less noise

--- scripts/laptop/battery_power.sh
+++ scripts/laptop/battery_power.sh
@@ -4,10 +4,15 @@ upower_output=$(upower -i $(upower -e | grep 'BAT'))
 
 battery_state=$(echo "$upower_output" | grep 'state' | awk '{print $2}')
 
-if [ "$battery_state" != "charging" ]
+if [ "$battery_state" = "charging" ]
 then
-    energy_rate=$(echo "$upower_output" | grep 'energy-rate' | awk '{print $2" W"}')
-    echo "$energy_rate"
-else
     echo "⚡"
+else
+    energy_rate=$(echo "$upower_output" | grep 'energy-rate' | awk '{print $2" W"}')
+    if [ "$energy_rate" = "0 W" ]
+    then
+        echo " "
+    else
+        echo "$energy_rate"
+    fi
 fi
